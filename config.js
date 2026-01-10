d615ef758b5bf8ebb7a2a80f302dea6aea148f73
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-01-10 15:33:35 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const configs = {
+  BOT_TOKEN: process.env.BOT_TOKEN || "8360280438:AAElkGIYMuKdANWAMxcuYqX9VyS-0hZdEAc",
+  OWNER_ID: process.env.OWNER_ID || [8462359928],
+  MONGO_DB: process.env.MONGO_DB || "mongodb+srv://QuizBot:QuizBot@cluster0.slbm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+};
+
+module.exports = configs;
